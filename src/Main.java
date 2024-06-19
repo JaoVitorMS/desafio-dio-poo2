8131c376f4e96b6d9fb227ba3cecb5be5b2e594c
João Vitor <joao.vitorms1509@gmail.com>
2024-06-19 17:52:05 -0300

    teste na Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main ( String[] args ) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Curso dio = new Curso();
+        dio.setTitulo("Curso dio");
+        dio.setDescricao("POO em java");
+        dio.setCargaHoraria(180);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Mentoria mentor = new Mentoria();
+        mentor.setTitulo("Spooky Houses");
+        mentor.setDescricao("Aula sobre o budismo tibetano");
+        mentor.setData(LocalDate.now());
+
     }
 }
